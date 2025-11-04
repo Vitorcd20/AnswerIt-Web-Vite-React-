e6f96ebd0ebe167fdca923744359c881d6d3a787
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-11-04 15:12:23 -0300

    feat:  app setup, rooms listing

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,21 @@
-import { Button } from "./components/ui/button";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+const queryClient = new QueryClient();
 
 export function App() {
-  return <div>
-    <Button>Save</Button>
-  </div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CreateRoom />} />
+          <Route path="/room/:roomId" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
